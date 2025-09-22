0f08f703e45ab2f150e5a51f23645872b69e7d2f
Rivaldo Mascarenhas <rivaldomascarenhas007@gmail.com>
2025-09-22 12:49:42 -0300

    Configures initial auth API routes
    Establishes foundational authentication endpoints and refines existing ones.
    
    Changes the login endpoint from GET to POST to adhere to security best practices for credential submission. Introduces new POST endpoints for user registration and token refreshing, setting up essential components for a complete authentication flow.

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -5,10 +5,18 @@ router_auth = APIRouter(
   tags=["auth"]
 )
 
-@router_auth.get("/login")
+@router_auth.get("/")
+async def root():
+  return {"message": "Hello World"}
+
+@router_auth.post("/login")
 async def login():
   return {"message": "Login"}
 
-@router_auth.get("/logout")
-async def logout():
-  return {"message": "Logout"}
+@router_auth.post("/register")
+async def register():
+  return {"message": "register"}
+
+@router_auth.post("/refresh")
+async def refresh():
+  return {"message": "refresh"}
